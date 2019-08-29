5d1fdd531ce7b23a44525a48c999722c33445d51
Frank Navarro <fnavarro7777@gmail.com>
2019-08-29 11:46:36 -0700

    Added Header Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -10,9 +11,10 @@ import StreamShow from './streams/StreamShow';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
