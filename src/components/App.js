778ba65777ae683a5cd18b6f9026ac88b676fa6c
Frank Navarro <fnavarro7777@gmail.com>
2019-08-29 11:35:54 -0700

    Wired up component pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 class App extends Component {
   render() {
@@ -7,6 +13,11 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
           </div>
         </BrowserRouter>
       </div>
