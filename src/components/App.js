1e80ed7de4b0bc6e53ca2a65f2a9f3717d42558e
Frank Navarro <fnavarro7777@gmail.com>
2019-09-12 11:45:38 -0700

    Created our own history obj for navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Router, Route} from 'react-router-dom';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -7,12 +7,13 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history';
 
 class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />
             <Route path="/" exact component={StreamList} />
@@ -21,7 +22,7 @@ class App extends Component {
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     );
   }
