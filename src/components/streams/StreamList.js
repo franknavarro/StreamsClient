26e0d692febaba81a18cf5690af2f14ec0deb986
Frank Navarro <fnavarro7777@gmail.com>
2019-09-12 11:23:53 -0700

    Show Admin Buttons for logged in user

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderAdminButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderStreamsList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -32,7 +44,10 @@ class StreamList extends Component {
 }
 
 const mapStateToProps = state => {
-  return {streams: Object.values(state.streams)};
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 };
 
 export default connect(
